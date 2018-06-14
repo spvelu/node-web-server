8f7769ce4d372af1094108b3fb008da960a5d14d
Sekar Velu <sekarvelu@Nishas-Mac-mini.local>
2018-06-13 20:44:09 -0700

    Initial Commit.

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static('./public'));
+
+app.get('/about', (req,res) => {
+    // res.send('About Page');
+        res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request.'
+    });
+});
+
+app.get('/', (req,res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page', 
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+
+});
